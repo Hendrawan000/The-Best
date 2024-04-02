2acce29917f41376f969ebcfc12dbdcf8732bf3b
Hendrawan000 <hendrawanlim30@gmail.com>
2024-04-02 20:11:46 +0700

    website the-best

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle Class Active
+// Toggle Class Active Untuk Hamburger Menu
 const navbarNav = document.querySelector
 ('.navbar-nav');
 // Ketika Hamburger Menu Di Klik
@@ -7,12 +7,73 @@ onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// Klik Di Luar Sidebar Untuk Menghilangkan Nav
-const Hamburger = document.querySelector
-('#hamburger-menu');
+// Toggle Class Active Untuk Search From
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle ('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// Toggle Class Active Untuk Shopping Cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle ('active');
+    e.preventDefault();
+};
+
+// Klik Di Luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e) {
-    if(!Hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
+});
+
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const ItemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+ItemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+    };
+});
+
+
+// Klik Tombol Close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+
+// Klik Di Luar Modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    };
+};
+
+alpine.store ('cart', {
+    items: cappucino, latte, americano,
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+        this.items.push(newItem);
+        console.log(this.items);
     }
 });
